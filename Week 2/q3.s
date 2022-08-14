6c74b7407828c355776a29117f0ca34c1abb0af1
PhoenixKing2501 <utsavbasu@outlook.com>
2022-08-14 22:11:59 +0530

    q3 is bs

--- Week 2/q3.s
+++ Week 2/q3.s
@@ -1,3 +1,57 @@
 # This program calculates transpose of a matrix of size (m * n)
 
+.data
+
+# program text constants
+prompt:
+.asciiz "Enter four positive integers m, n, a and r:\n"
+
+matrixA:
+.asciiz "Matrix A:\n"
+
+matrixB:
+.asciiz "Matrix B:\n"
+
+space:
+.asciiz " "
+
+newline:
+.asciiz "\n"
+
+.text
+.globl main
+
+# main program
+# m:	$s0
+# n:	$s1
+# a:	$s2
+# r:	$s3
+# m*n:	$s4
+
+main:
+	li      $v0,    4                  # system call #4 - print string
+	la      $a0,    prompt
+	syscall                            # execute
+
+	li      $v0,    5                  # $v0 = 5
+	syscall                            # input m
+	move    $s0,    $v0                # $s0 = $v0
+
+	li      $v0,    5                  # $v0 = 5
+	syscall                            # input n
+	move    $s1,    $v0                # $s1 = $v0
+
+	li      $v0,    5                  # $v0 = 5
+	syscall                            # input a
+	move    $s2,    $v0                # $s2 = $v0
+
+	li      $v0,    5                  # $v0 = 5
+	syscall                            # input r
+	move    $s3,    $v0                # $s3 = $v0
+
+	mul     $s4,    $s0,    $s1        # $s4 = $s0 * $s1
+
+	
+
+
 
