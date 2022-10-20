657c8c18e928d4c9451bc7cde7dcf5c6a55379f7
PhoenixKing2501 <utsavbasu@outlook.com>
2022-10-20 15:19:35 +0530

    PC modified

--- KGP mini-RISC/program_counter.v
+++ KGP mini-RISC/program_counter.v
@@ -1,12 +1,14 @@
+`timescale 1ns / 1ps
+
 module program_counter (
 	input [31:0] PC,
-	input clock,
-	input reset,
+	input clk,
+	input rst,
 	output reg [31:0] PC_new
 );
 
-	always @(posedge clock or posedge reset) begin
-		if (reset) begin
+	always @(posedge clk or posedge rst) begin
+		if (rst) begin
 			PC = -32'd0;
 		end else begin
 			PC = PC_new;
