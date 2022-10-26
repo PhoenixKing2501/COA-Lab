0f01a65b30d49e7e55de0a1c8b8b22481fd09694
PhoenixKing2501 <utsavbasu@outlook.com>
2022-10-26 13:26:39 +0530

    program_counter.v modified

--- KGP mini-RISC/program_counter.v
+++ KGP mini-RISC/program_counter.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 
 module program_counter (
-	input [31:0] PC,
+	input [31:0] PC_new
 	input clk,
 	input rst,
-	output reg [31:0] PC_new
+	output [31:0] PC,
 );
 
 	always @(posedge clk or posedge rst) begin
 		if (rst) begin
-			PC = -32'd0;
+			PC = 32'd0;
 		end else begin
 			PC = PC_new;
 		end
